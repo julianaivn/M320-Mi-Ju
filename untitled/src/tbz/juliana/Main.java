db03c82ab00b1fe21647343ff8ec37f626f1943a
juliana <142787008+julianaivn@users.noreply.github.com>
2024-11-11 15:46:59 +0100

    einzel aufgabe 1 - juliana

---
+++ untitled/src/tbz/juliana/Main.java
@@ -0,0 +1,29 @@
+package tbz.juliana;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("password generator");
+        System.out.println("------------------");
+        System.out.println("how long should the password be?");
+
+        int length = scanner.nextInt();
+        System.out.println("password with " + length + " characters");
+        System.out.println(generator(length));
+
+    }
+
+    public static String generator(int length){
+        StringBuilder pswrd = new StringBuilder();
+        Random random = new Random();
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw0123456789?!@$*";
+
+        for(int i = 0; i < length; i++){
+            pswrd.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return pswrd.toString();
+    }
+}
