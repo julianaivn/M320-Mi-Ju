fe2e58bf95f539f4fa6cf86386974038f222574e
juliana <142787008+julianaivn@users.noreply.github.com>
2024-11-11 16:16:00 +0100

    added option to chose the variety of characters

--- Repetition M319/PasswordGenerator.java
+++ Repetition M319/PasswordGenerator.java
@@ -9,19 +9,44 @@ public class PasswordGenerator {
         System.out.println("how long should the password be?");
 
         int length = scanner.nextInt();
+        String characters = characterRange();
         System.out.println("password with " + length + " characters");
-        System.out.println(generator(length));
+        System.out.println(generator(length, characters));
 
     }
 
-    public static String generator(int length){
+    private static String generator(int length, String characters){
         StringBuilder pswrd = new StringBuilder();
         Random random = new Random();
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw0123456789?!@$*";
 
         for(int i = 0; i < length; i++){
             pswrd.append(characters.charAt(random.nextInt(characters.length())));
         }
         return pswrd.toString();
+    }
+
+    private static String characterRange(){
+        StringBuilder characters = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("do you want lowercase letters? (y/n)");
+        String input =  scanner.nextLine();
+        if(input.equals("y")){
+            characters.append("abcdefghijklmnopqrstuvwxyz");
+        }
+
+        System.out.println("do you want uppercase letters? (y/n)");
+        input =  scanner.nextLine();
+        if(input.equals("y")){
+            characters.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        }
+
+        System.out.println("do you want special characters like '?!@$*'? (y/n)");
+        input =  scanner.nextLine();
+        if(input.equals("y")){
+            characters.append("?!@$*");
+        }
+
+        return characters.toString();
     }
 }
