601f96c935bc8603afd13f4cf27bdb480e6e366a
Michellevp <michelle@tie-international.com>
2024-12-09 14:32:56 +0100

    Updated mannschaft entity

---
+++ V2/src/ch.tbz.m320/Mannschaft.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Mannschaft {
+    private ArrayList<Spieler> spielerListe;
+
+    public Mannschaft() {
+        spielerListe = new ArrayList<>();
+    }
+
+    public void addSpieler(Spieler spieler) {
+        spielerListe.add(spieler);
+    }
+
+    public void showTeam() {
+        for (Spieler spieler : spielerListe) {
+            System.out.println(spieler.spielen());
+        }
+    }
+}
