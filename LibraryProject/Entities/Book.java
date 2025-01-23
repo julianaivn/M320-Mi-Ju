3d01587dc545d9c1a19977b5f0111e7a6ce987d5
Michellevp <michelle@tie-international.com>
2025-01-23 23:06:35 +0100

    Creation of variables and constructor

---
+++ LibraryProject/Entities/Book.java
@@ -0,0 +1,15 @@
+package Entities;
+
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+}
